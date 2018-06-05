fe5333ff325b0ed0638a1e723e2e3faeb5195e1c
Vanessa Azócar <v.azocar.adasme@gmail.com>
2018-06-04 23:49:24 -0300

    manera de recorrer los elementos del array al revés

--- assets/app.js
+++ assets/app.js
@@ -1,2 +1,20 @@
 window.onload = function (){
+/*Crea una función que invierta el orden de los elementos de un array. Completa este reto de dos maneras: en una, el array invertido es un nuevo array; en la segunda, modifica el mismo array que se proporciona como argumento. Investiga sobre el método reverse. ¿Qué puedes concluir? */
+
+//ARRAY INVERTIDO ES UN NUEVO ARRAY
+
+/* asi se ven las letras en orden de izq a derecha
+let vowels = ["a","e","i","o","u",];
+const size = vowels.length; //5
+for(var i=0; i<vowels.length; i++){
+this.console.log(vowels[i]);
+}*/
+
+let vowels = ["a","e","i","o","u",];
+const size = vowels.length; //5
+const lastPosition = size-1; //por si crece el largo de vowels siempre tomara el ultimo dato
+for(var i=lastPosition; i>0; i--){
+console.log(vowels[i]);
+}
+
 }
