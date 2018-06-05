b301e58f4b41f87ace96f836010987b1ce9dbe31
Vanessa Azócar <v.azocar.adasme@gmail.com>
2018-06-05 00:07:14 -0300

    otra forma de mostrar los elementos array al reves

--- assets/app.js
+++ assets/app.js
@@ -10,11 +10,27 @@ for(var i=0; i<vowels.length; i++){
 this.console.log(vowels[i]);
 }*/
 
-let vowels = ["a","e","i","o","u",];
+/* asi se recorre el array de orden derecha a izq
+const vowels = ["a","e","i","o","u",];
 const size = vowels.length; //5
 const lastPosition = size-1; //por si crece el largo de vowels siempre tomara el ultimo dato
 for(var i=lastPosition; i>0; i--){
 console.log(vowels[i]);
+} */
+const vowels = ["a","e","i","o","u",];
+const numbers = ["one","two","three"];
+const reverse = function(array) {
+  const newArray = [];
+  let size = array.length;
+  const lastPosition = size -1;
+  for (var i= lastPosition; i>=0; i--){
+    newArray.push(array[i]);
+  }
+return newArray;
 }
+console.log (reverse(vowels));
+console.log (vowels);
 
+console.log (reverse(numbers));
+console.log (numbers);
 }
